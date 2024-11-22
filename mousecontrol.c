faab6c779fc98bf9c5e8bfca358971886e5581f2
Aaron <117947303+aaronbehan@users.noreply.github.com>
2024-11-22 16:38:49 +0000

    Update mousecontrol.c
    Multiple units can now be on screen and controlled.
    Mouse wheel now controls height of camera.

--- mousecontrol.c
+++ mousecontrol.c
@@ -1,17 +1,22 @@
 #include "raylib.h"
-#include "raymath.h"  // possibly not necessary
+#include "raymath.h"
 
 #define FLT_MAX     340282346638528859811704183484516925440.0f     // Maximum value of a float, from bit pattern 01111111011111111111111111111111. I think Ray used this in the event of a rare edge case
+
+//  debugging time. units only have to be selected once for their moveunit status to be set permantly to true
 
 // Player units which can be controlled
 typedef struct Unit {
     Vector3 unitPos;  // X = East and West. Y = Up and Down. Z = North and South.
+    Vector3 waypoint; 
     Vector3 unitSize;
     float unitSpeed;
     bool selected;
+    bool moveUnit;
+    Color color;
 } Unit;
 
-BoundingBox createBoundingBox(Vector3 startPoint, Vector3 endPoint) // Creates a bounding box using its two corners
+BoundingBox createBoundingBox(Vector3 startPoint, Vector3 endPoint) // Creates a bounding box using its two furthest corners
 {
     BoundingBox boundingBox = { 0 };
 
@@ -19,7 +24,7 @@ BoundingBox createBoundingBox(Vector3 startPoint, Vector3 endPoint) // Creates a
     else if (startPoint.x > endPoint.x) boundingBox.min.x = endPoint.x, boundingBox.max.x = startPoint.x;  // Unexpected 
     
     if (startPoint.z <= endPoint.z) boundingBox.min.z = startPoint.z, boundingBox.max.z = endPoint.z;  // Expected mouse drag direction
-    else if (startPoint.z > endPoint.z) boundingBox.min.z = endPoint.z, boundingBox.max.z = startPoint.z;  // Unexpected direction
+    else if (startPoint.z > endPoint.z) boundingBox.min.z = endPoint.z, boundingBox.max.z = startPoint.z;  // Unexpected
 
     boundingBox.min.y = 0.0f;
     boundingBox.max.y = 0.5f; // Arbitrary height value 
@@ -32,13 +37,12 @@ int main(void)
     // Screen
     const int screenWidth = 1400;
     const int screenHeight = 850;
-    InitWindow(screenWidth, screenHeight, "raylib example");
+    InitWindow(screenWidth, screenHeight, "raylib concepts and experimentation");
 
     // Camera
     Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
     float cameraSpeed = 0.5;
     Ray ray = { 0 };
-
 
     // Ground quad. Allows mouse rays to collide with the ground and return a 3D coordinate
     Vector3 g0 = (Vector3){ -50.0f, 0.0f, -50.0f };
@@ -50,9 +54,13 @@ int main(void)
     RayCollision groundCollision = { 0 };  // For casting a ray at the GROUND in order to return 3D coordinates 
     Vector3 wayPoint = { 0 };  // Saves a 3D coordinate. Used for unit control 
 
-    // Player
-    Unit playerUnit = { { 0.0f, 1.0f, -4.0f }, { 1.0f, 2.0f, 1.0f }, 0.05f, false}; 
-    bool moveUnit = false;  // will turn true when wayPoint is set and will be set to false when wayPoint coordinates are matched by unit coordinates
+    #define NUMBER_OF_PLAYER_UNITS 2
+
+    // Player controlled units
+    Unit playerUnit1 = { { 0.0f, 1.0f, -4.0f }, { 0.0f, 1.0f, -4.0f }, { 1.0f, 2.0f, 1.0f }, 0.01f, false, false, GRAY}; 
+    Unit playerUnit2 = { { 4.0f, 1.0f, 3.0f }, { 4.0f, 1.0f, 3.0f }, { 1.0f, 2.0f, 1.0f }, 0.01f, false, false, ORANGE}; 
+
+    Unit playerUnits[NUMBER_OF_PLAYER_UNITS] = {playerUnit1, playerUnit2};
 
     // For drawing a visual aid rectangle with the mouse
     Vector2 saveMousePos2D = { 0 };
@@ -71,9 +79,11 @@ int main(void)
         if (IsKeyDown(KEY_W)) camera.position.z -= cameraSpeed, camera.target.z -= cameraSpeed;
         if (IsKeyDown(KEY_A)) camera.position.x -= cameraSpeed, camera.target.x -= cameraSpeed;
         if (IsKeyDown(KEY_S)) camera.position.z += cameraSpeed, camera.target.z += cameraSpeed;
-        if (IsKeyDown(KEY_D)) camera.position.x += cameraSpeed, camera.target.x += cameraSpeed;   
-        if (IsKeyDown(KEY_Q)) camera.position.y += cameraSpeed, camera.target.y += cameraSpeed;  // CAMERA UP
-        if (IsKeyDown(KEY_E)) camera.position.y -= cameraSpeed, camera.target.y -= cameraSpeed;  // CAMERA DOWN
+        if (IsKeyDown(KEY_D)) camera.position.x += cameraSpeed, camera.target.x += cameraSpeed;
+        camera.position.y -= (GetMouseWheelMove()*cameraSpeed);  // CAMERA UP
+        camera.target.y -= (GetMouseWheelMove()*cameraSpeed);  
+        camera.position.z -= (GetMouseWheelMove()*0.6);
+
         if ((IsKeyPressed(KEY_SPACE)) && (!showDebug)) showDebug = true;
         if ((IsKeyPressed(KEY_P)) && (showDebug)) showDebug = false;
         
@@ -90,34 +100,39 @@ int main(void)
         // MOUSE CONTROLS ----------------------------------------------------------------------
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
-            // At the start of each click, we assume that no object collision has occurred until we re-evaluate with below functions
+            // At the start of each click, we assume that no object collision has occurred until we evaluate with below functions
             objectCollision.hit = false;
 
             // Saving coordinate to start as the top left corner of a visual aid rectangle
             saveMousePos2D.x = GetMousePosition().x;
             saveMousePos2D.y = GetMousePosition().y;  
             
-            // Deciding where to put ONE CORNER of the cuboid which will we will use to define a bounding box. Y coordinate is static
+            // Deciding where to put ONE CORNER of the cuboid which will we will use to define bounding box. Y coordinate is static
             save3DMouseStartPos.x = groundCollision.point.x;
             save3DMouseStartPos.z = groundCollision.point.z;
+            
+            // Check collision between ray and each player unit (except the ground)
+            for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++) {
+                objectCollision = GetRayCollisionBox(ray,
+                                        (BoundingBox){(Vector3){ playerUnits[i].unitPos.x - playerUnits[i].unitSize.x/2, playerUnits[i].unitPos.y - playerUnits[i].unitSize.y/2, playerUnits[i].unitPos.z - playerUnits[i].unitSize.z/2 },
+                                        (Vector3){ playerUnits[i].unitPos.x + playerUnits[i].unitSize.x/2, playerUnits[i].unitPos.y + playerUnits[i].unitSize.y/2, playerUnits[i].unitPos.z + playerUnits[i].unitSize.z/2 }});
 
-            // Check collision between ray and an object (except the ground)
-            objectCollision = GetRayCollisionBox(ray,
-                                        (BoundingBox){(Vector3){ playerUnit.unitPos.x - playerUnit.unitSize.x/2, playerUnit.unitPos.y - playerUnit.unitSize.y/2, playerUnit.unitPos.z - playerUnit.unitSize.z/2 },
-                                        (Vector3){ playerUnit.unitPos.x + playerUnit.unitSize.x/2, playerUnit.unitPos.y + playerUnit.unitSize.y/2, playerUnit.unitPos.z + playerUnit.unitSize.z/2 }});
+                playerUnits[i].selected = objectCollision.hit;
+            }
         }
 
         if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) 
         {
             // Defining where the other corner of our bounding box will be
             save3DMouseEndPos.x = groundCollision.point.x;
-            save3DMouseEndPos.z = groundCollision.point.z;  // set these to 0 each time we iterate through the loop otherwise the boundingbox may persist and continue to register collision
+            save3DMouseEndPos.z = groundCollision.point.z; 
 
             BoundingBox boundingBox = createBoundingBox(save3DMouseStartPos, save3DMouseEndPos);
-
-            if (CheckCollisionBoxes(boundingBox, (BoundingBox){(Vector3){ playerUnit.unitPos.x - playerUnit.unitSize.x/2, playerUnit.unitPos.y - playerUnit.unitSize.y/2, playerUnit.unitPos.z - playerUnit.unitSize.z/2 },
-                                            (Vector3){ playerUnit.unitPos.x + playerUnit.unitSize.x/2, playerUnit.unitPos.y + playerUnit.unitSize.y/2, playerUnit.unitPos.z + playerUnit.unitSize.z/2 }})) objectCollision.hit = true;
             
+            for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++) {
+                if (CheckCollisionBoxes(boundingBox, (BoundingBox){(Vector3){ playerUnits[i].unitPos.x - playerUnits[i].unitSize.x/2, playerUnits[i].unitPos.y - playerUnits[i].unitSize.y/2, playerUnits[i].unitPos.z - playerUnits[i].unitSize.z/2 },
+                                (Vector3){ playerUnits[i].unitPos.x + playerUnits[i].unitSize.x/2, playerUnits[i].unitPos.y + playerUnits[i].unitSize.y/2, playerUnits[i].unitPos.z + playerUnits[i].unitSize.z/2 }})) playerUnits[i].selected = true;
+            }                               
         }
 
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
@@ -126,28 +141,25 @@ int main(void)
             DrawRectangleLines(saveMousePos2D.x, saveMousePos2D.y, GetMousePosition().x - saveMousePos2D.x, GetMousePosition().y - saveMousePos2D.y, BLUE);
         }
 
-        if ((IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) && (objectCollision.hit))// Move collided units with the mouse
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))  // Move selected units with RMB. This is also how we will select targets for my units
         {
-            // this method ties in movement speed with the framerate. possibly bad form!!!! once we have a timer, we will tie it to that
-            if ((playerUnit.unitPos.x != groundCollision.point.x) || (playerUnit.unitPos.z != groundCollision.point.z)) 
+            for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++)  // ITERATE THROUGH ALL UNITS
             {
-                moveUnit = true;
-                wayPoint.x = groundCollision.point.x;
-                wayPoint.z = groundCollision.point.z;
+                if (playerUnits[i].selected)  // ONLY WANT TO ALLOW SELECTED UNITS NEW WAYPOINTS
+                {
+                        playerUnits[i].waypoint.x = groundCollision.point.x;  // Setting a static coordinate for units to follow
+                        playerUnits[i].waypoint.z = groundCollision.point.z;
+                }
             }
         }
         // END OF MOUSE CONTROLS ----------------------------------------------------------------
 
-        if (moveUnit) 
-        {   // Checks to see whether the unit has reached the intended coordinates, in which case, moveUnit = false
-            if ((playerUnit.unitPos.x == groundCollision.point.x) && (playerUnit.unitPos.z == groundCollision.point.z)) moveUnit = false;
-            else 
-            {
-                if (playerUnit.unitPos.x < wayPoint.x) playerUnit.unitPos.x += playerUnit.unitSpeed;
-                if (playerUnit.unitPos.x > wayPoint.x) playerUnit.unitPos.x -= playerUnit.unitSpeed;
-                if (playerUnit.unitPos.z < wayPoint.z) playerUnit.unitPos.z += playerUnit.unitSpeed;
-                if (playerUnit.unitPos.z > wayPoint.z) playerUnit.unitPos.z -= playerUnit.unitSpeed;
-            }
+        for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++) // FOR MOVING SELECTED UNITS
+        {
+            if (playerUnits[i].unitPos.x < playerUnits[i].waypoint.x) playerUnits[i].unitPos.x += playerUnits[i].unitSpeed;  // these increments are large to achieve equillibrium between waypoint and unit coordinates
+            if (playerUnits[i].unitPos.x > playerUnits[i].waypoint.x) playerUnits[i].unitPos.x -= playerUnits[i].unitSpeed;
+            if (playerUnits[i].unitPos.z < playerUnits[i].waypoint.z) playerUnits[i].unitPos.z += playerUnits[i].unitSpeed;
+            if (playerUnits[i].unitPos.z > playerUnits[i].waypoint.z) playerUnits[i].unitPos.z -= playerUnits[i].unitSpeed;
         }
 
         // Draw
@@ -157,32 +169,31 @@ int main(void)
 
             BeginMode3D(camera);
 
-                // Draw unit
-                if (objectCollision.hit)  
-                {
-                    DrawCube(playerUnit.unitPos, playerUnit.unitSize.x, playerUnit.unitSize.y, playerUnit.unitSize.z, RED);
-                    DrawCubeWires(playerUnit.unitPos, playerUnit.unitSize.x, playerUnit.unitSize.y, playerUnit.unitSize.z, MAROON);
+                // Highlight them if they are selected/hit
+                for (int i = 0; i < NUMBER_OF_PLAYER_UNITS; i++) {  // iterate through the units
 
-                    DrawCubeWires(playerUnit.unitPos, playerUnit.unitSize.x + 0.2f, playerUnit.unitSize.y + 0.2f, playerUnit.unitSize.z + 0.2f, GREEN);
-                }
-                else
-                {
-                    DrawCube(playerUnit.unitPos, playerUnit.unitSize.x, playerUnit.unitSize.y, playerUnit.unitSize.z, GRAY);
-                    DrawCubeWires(playerUnit.unitPos, playerUnit.unitSize.x, playerUnit.unitSize.y, playerUnit.unitSize.z, DARKGRAY);
+                    DrawCube(playerUnits[i].unitPos, playerUnits[i].unitSize.x, playerUnits[i].unitSize.y, playerUnits[i].unitSize.z, playerUnits[i].color);  // unit 1
+                    DrawCubeWires(playerUnits[i].unitPos, playerUnits[i].unitSize.x, playerUnits[i].unitSize.y, playerUnits[i].unitSize.z, DARKGRAY);
+                
+                    if (playerUnits[i].selected) // to check which of them are selected. then draw a wireframe around those that are
+                    {
+                        DrawCubeWires(playerUnits[i].unitPos, playerUnits[i].unitSize.x, playerUnits[i].unitSize.y, playerUnits[i].unitSize.z, MAROON);
+                        DrawCubeWires(playerUnits[i].unitPos, playerUnits[i].unitSize.x + 0.2f, playerUnits[i].unitSize.y + 0.2f, playerUnits[i].unitSize.z + 0.2f, GREEN);
+                    }
                 }
 
                 // This lets me see where the invisible bounding box will be created for debugging purposes
-                // DrawCube((Vector3){save3DMouseStartPos.x, 0.0f, save3DMouseStartPos.z}, 0.1f, 0.1f, 0.1f, GRAY);
-                // DrawCube((Vector3){save3DMouseEndPos.x, 0.0f, save3DMouseEndPos.z}, 0.1f, 0.1f, 0.1f, GRAY);
+                DrawCube((Vector3){save3DMouseStartPos.x, 0.0f, save3DMouseStartPos.z}, 0.1f, 0.1f, 0.1f, GRAY);
+                DrawCube((Vector3){save3DMouseEndPos.x, 0.0f, save3DMouseEndPos.z}, 0.1f, 0.1f, 0.1f, GRAY);
 
-                // Vector3 normalEnd;
-                // normalEnd.x = groundCollision.point.x + groundCollision.normal.x;
-                // normalEnd.y = groundCollision.point.y + groundCollision.normal.y;
-                // normalEnd.z = groundCollision.point.z + groundCollision.normal.z;
+                Vector3 normalEnd;
+                normalEnd.x = groundCollision.point.x + groundCollision.normal.x;
+                normalEnd.y = groundCollision.point.y + groundCollision.normal.y;
+                normalEnd.z = groundCollision.point.z + groundCollision.normal.z;
 
                 // CREATING A LITTLE CUBE THAT STICKS TO THE GROUND AND FOLLOWS THE MOUSE
-                // DrawCube(groundCollision.point, 0.1f, 0.1f, 0.1f, GRAY);
-                // DrawLine3D(groundCollision.point, normalEnd, RED);
+                DrawCube(groundCollision.point, 0.1f, 0.1f, 0.1f, GRAY);
+                DrawLine3D(groundCollision.point, normalEnd, RED);
                 
                 DrawGrid(10, 1.0f);        // Draw a grid (slices, spacing)
 
@@ -193,19 +204,9 @@ int main(void)
             if (showDebug) 
             {
                 DrawFPS(10, 10);
-                DrawText(TextFormat("GetMousePosition()X = %f,\n\nY= %f", GetMousePosition().x, GetMousePosition().y), 30, 20, 30, LIGHTGRAY);
-                DrawText(TextFormat("save3DStartPosX = %f,\n\nZ= %f", save3DMouseStartPos.x, save3DMouseStartPos.z), 30, 140, 30, LIGHTGRAY);
-                DrawText(TextFormat("save3DEndPosX = %f,\n\nZ = %f", save3DMouseEndPos.x, save3DMouseEndPos.z), 30, 250, 30, LIGHTGRAY);
-                DrawText(TextFormat("ray.direction.x = %f,\n\nray.direction.y = %f\n\nray.direction.z = %f", ray.direction.x, ray.direction.y, ray.direction.z), 30, 350, 30, LIGHTGRAY);
-                DrawText(TextFormat("Hit Pos: %3.2f %3.2f %3.2f",
-                                    groundCollision.point.x,
-                                    groundCollision.point.y,
-                                    groundCollision.point.z), 10, 70 + 15, 10, BLACK);
-
-                DrawText(TextFormat("Hit Norm: %3.2f %3.2f %3.2f",
-                                    groundCollision.normal.x,
-                                    groundCollision.normal.y,
-                                    groundCollision.normal.z), 10, 70 + 30, 10, BLACK);
+                DrawText(TextFormat("playerUnits[0].moveUnit = %d,\n\nplayerUnits[0].selected= %d", playerUnits[0].moveUnit, playerUnits[0].selected), 30, 20, 30, LIGHTGRAY);
+                DrawText(TextFormat("playerUnits[0].unitPos.x = %f,\n\nplayerUnits[0].unitPos.z= %f", playerUnits[0].unitPos.x, playerUnits[0].unitPos.z), 30, 220, 30, LIGHTGRAY);
+                DrawText(TextFormat("wayPointX = %f,\n\nZ= %f", wayPoint.x, wayPoint.z), 30, 140, 30, LIGHTGRAY);
             }
 
         EndDrawing();
